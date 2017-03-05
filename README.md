525dcf84a68fbcac1ee6f1c355644a033247d3b5
klocode <kendrick.t.lo@gmail.com>
2017-03-05 12:36:14 -0500

    adding heroku link to readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 
-This repo contains code for an API for Chirp, a Twitter clone
+This repo contains code for an API for Chirp, a Twitter clone hosted on heroku at:
+* https://shielded-taiga-17184.herokuapp.com/
--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,8 +2,6 @@ class FollowsController < ApplicationController
   before_action :require_user, only: [:create]
 
 
-  # if else?
-  # it's not changing follow
   def create
     current_user.toggle_follow!(User.find_by(id: params[:id]))
     if current_user.follows?(User.find_by(id: params[:id]))
